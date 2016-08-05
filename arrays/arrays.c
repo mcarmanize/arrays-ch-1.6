5b546e879595a014f97aa9ee8d726e92bf838bd6
namrak <namrak@gmail.com>
2016-08-05 10:22:48 -0600

    removed new line and spacing

--- arrays/arrays.c
+++ arrays/arrays.c
@@ -12,13 +12,13 @@ main()
 
 	while ((c = getchar()) != EOF)
 		if (c >= '0' && c <= '9')
-			++ndigit[c - '0'];
+			++ndigit[c-'0'];
 		else if (c == ' ' || c == '\n' || c == '\t')
 			++nwhite;
 		else
 			++nother;
 
-	printf("\ndigits =");
+	printf("digits =");
 	for (i = 0; i < 10; ++i)
 		printf(" %d", ndigit[i]);
 	printf(", whitespace = %d, other = %d\n",
